182096069baa5cd5152c33ee74ee990b77fa03d2
afgoll <aflahmonuzz@gmail.com>
2025-09-15 06:58:41 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -107,12 +107,11 @@ async def start(client, message):
             InlineKeyboardButton(f'🖥️ Oᴛᴛ Uᴩᴅᴀᴛᴇꜱ Cʜᴀɴɴᴇʟ 🖥️', url='https://t.me/+JObo3SXOiZRlNzll')
                 ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_photo(
-            photo=random.choice(PICS),
-            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
-            reply_markup=reply_markup,
-            parse_mode=enums.ParseMode.HTML,
-        )
+        await message.reply_text(
+    script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+    reply_markup=reply_markup,
+    parse_mode=enums.ParseMode.HTML
+)
         return
         
     if message.command[1].startswith("reff_"):
